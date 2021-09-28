45d8265bf707284fb0ce83209d0cc25fa921b16b
codersergg <sergey.i.chopin@gmail.com>
2021-09-28 20:41:14 +0300

    first commit

---
+++ src/main/java/com/example/democrudjwt/DemoCrudJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.democrudjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrudJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/democrudjwt/DemoCrudJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democrudjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
